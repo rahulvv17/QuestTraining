65e8de0ecdb3d54da04d2ac54e5033c54b3c34ed
Rahul  vv <rahulrachu21@gmail.com>
2024-10-16 08:56:51 +0530

    Initial commit

---
+++ Assessments/Assessment1/HospitalManagementSystem/viva/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTwo
+{
+    internal class Program
+    {
+
+
+
+
+        static void AddCreditCard()
+        {
+            var cc = new CreditCard();
+
+            Console.WriteLine("Enter Card Holder Name:");
+            cc.CardHolderName = Console.ReadLine();
+
+            Console.WriteLine("Enter Card Number:");
+            cc.CardNumber = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Expiry Month:");
+            cc.ExpiryMonth = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Expiry Year:");
+            cc.ExpiryYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter CVC:");
+            cc.Cvc = int.Parse(Console.ReadLine());
+
+            creditCards.Add(cc);
+            Console.WriteLine("Credit Card added successfully!");
+        }
+        static List<CreditCard> creditCards = new List<CreditCard>();
+        static void Main(string[] args)
+        {
+            var cc = new CreditCard();
+            int choice;
+            while (true)
+            {
+
+
+                Console.WriteLine("Enter the card Name");
+                cc.CardHolderName = Console.ReadLine();
+
+                Console.WriteLine("Enter the card number");
+                cc.CardNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the expiry month");
+                cc.ExpiryMonth = byte.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the expiry year");
+                cc.ExpiryYear = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter CVC");
+                cc.Cvc = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Choice");
+                choice = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n1. Add Credit Card");
+                Console.WriteLine("2. Update Credit Card");
+                Console.WriteLine("3. Delete Credit Card");
+                Console.WriteLine("4. Search Credit Card by Number");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddCreditCard();
+                        break;
+                    case 2:
+                        return;
+                        //    UpdateCreditCard();
+                        //    break;
+                        //case 3:
+                        //    DeleteCreditCard();
+                        //    break;
+                        //case 4:
+                        //    SearchByCardNumber();
+                        //    break;
+                        //case 5:
+                        //    Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+
+
+
+
+            }
+
+
+
+
+
+
+            Console.WriteLine(cc);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CreditCard> list = new List<CreditCard>();
+            while (true)
+            {
+                Console.WriteLine("1-add,2-update,3-search,4-delete");
+                Console.WriteLine("enter choice");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("enter cardholder name");
+                        var Name = Console.ReadLine();
+                        Console.WriteLine("Enter card number");
+                        var CardNumber = long.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter expiry (MM/YY)");
+                        var expiry = Console.ReadLine();
+
+                        var details = new CreditCard
+                        {
+                            CardHolderName = Name,
+                            CardNumber = CardNumber,
+                            ExpiryMonth = byte.Parse(expiry.Split('/')[0]),
+                            ExpiryYear = byte.Parse(expiry.Split('/')[1])
+                        };
+                        list.Add(details);
+                        Console.WriteLine(details);
+                        break;
+
+                    case 2:
+                        CreditCard updatecard = null;
+                        Console.WriteLine("enter card number for updation");
+                        var cardnumber = long.Parse(Console.ReadLine());
+
+                        foreach (var card in list)
+                        {
+                            if (card.CardNumber == cardnumber)
+                            {
+                                updatecard = card;
+                                break;
+                            }
+                        }
+
+                        if (updatecard != null)
+                        {
+                            Console.WriteLine("Enter new card number");
+                            var newcardnumber = long.Parse(Console.ReadLine());
+                            updatecard.CardNumber = newcardnumber;
+                            Console.WriteLine("card updated successfully");
+                        }
+                        Console.WriteLine("updated elements are " + updatecard);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("enter card number for searching");
+                        var searchnum = long.Parse(Console.ReadLine());
+
+                        CreditCard searchnumber = null;
+                        foreach (var card in list)
+                        {
+                            if (card.CardNumber == searchnum)
+                            {
+                                searchnumber = card;
+                                break;
+                            }
+                        }
+                        Console.WriteLine("Elements are " + searchnumber);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("enter card number");
+                        var CardNumbernForDeletion = long.Parse(Console.ReadLine());
+                        CreditCard delnumber = null;
+                        foreach (var card in list)
+                        {
+                            if (card.CardNumber == CardNumbernForDeletion)
+                            {
+                                delnumber = card;
+                                break;
+                            }
+                        }
+                        if (delnumber != null)
+                        {
+                            list.Remove(delnumber);
+                            Console.WriteLine("successfully deleted");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
